bcf16247ef826b319636d6155d726d2372a83843
Alex Churin <qitip@yandex.ru>
2022-08-13 02:13:46 +0300

    init heroku add  asyncpg add inlinemode import add hashlib c add servetdt answers add import requests fix id answers2 cacheTime 200

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ async def inline_echo(inline_query: InlineQuery):
                 input_message_content={'message_text':models},
             ))
         # don't forget to set cache_time=1 for testing (default is 300s or 5m)
-    await bot.answer_inline_query(inline_query.id, results=items, cache_time=20)
+    await bot.answer_inline_query(inline_query.id, results=items, cache_time=200)
 
 
 @dp.message_handler()
