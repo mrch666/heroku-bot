46e850b1777d1a08aa443222187f3118ba81173b
Alex Churin <qitip@yandex.ru>
2022-08-13 00:33:59 +0300

    init heroku add db postgress asyncpg add inlinemode import

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from aiogram import types
+from aiogram.types import InlineQuery, InputTextMessageContent, InlineQueryResultArticle
 from aiogram.utils.executor import start_webhook
 from config import bot, dp, WEBHOOK_URL, WEBHOOK_PATH, WEBAPP_HOST, WEBAPP_PORT
 from db import database
