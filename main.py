cdd1108243c17f597f7bf9c4d57daa70acedc92f
Alex Churin <qitip@yandex.ru>
2022-08-13 00:30:15 +0300

    init heroku add db postgress asyncpg add inlinemode

--- main.py
+++ main.py
@@ -27,6 +27,25 @@ async def read(user_id):
                                        values={'telegram_id': user_id})
     return [next(result.values()) for result in results]
 
+@dp.inline_handler()
+async def inline_echo(inline_query: InlineQuery):
+    # id affects both preview and content,
+    # so it has to be unique for each result
+    # (Unique identifier for this result, 1-64 Bytes)
+    # you can set your unique id's
+    # but for example i'll generate it based on text because I know, that
+    # only text will be passed in this example
+    text = inline_query.query or 'echo'
+    input_content = InputTextMessageContent(text)
+    result_id: str = hashlib.md5(text.encode()).hexdigest()
+    item = InlineQueryResultArticle(
+        id=result_id,
+        title=f'Result {text!r}',
+        input_message_content=input_content,
+    )
+    # don't forget to set cache_time=1 for testing (default is 300s or 5m)
+    await bot.answer_inline_query(inline_query.id, results=[item], cache_time=1)
+
 
 @dp.message_handler()
 async def echo(message: types.Message):
